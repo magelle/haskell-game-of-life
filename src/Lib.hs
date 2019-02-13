12f2d5df441be10c71959bccf6d210b585c7dc4a
Maxime GELLE <maxime.gelle@gmail.com>
2019-02-13 14:00:19 +0100

    Trying to find the neighbors

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,18 +7,37 @@ instance Show Cell where
     show Alive = "O" 
     show Dead = "_" 
 
-data Grid = Grid [Line]
+data Grid = Grid [Line] deriving (Eq)
 instance Show Grid where
     show (Grid lines) = intercalate "\n" (map show lines)
 
-data Line = Line [Cell]
+data Line = Line [Cell] deriving (Eq)
 instance Show Line where
     show (Line cells) = foldl (++) "" (map show cells)
 
-nextGen :: Cell -> Int -> Cell
-nextGen Alive 2 = Alive
-nextGen _ 3 = Alive
-nextGen _ _ = Dead
+neighborhood :: Int -> [Cell] -> [Cell]
+neighborhood target = takeFrom (target - 1) 3 
+
+removeMiddle :: [a] -> [a]
+removeMiddle (a:b:tail) = a:tail
+
+neighbors :: Int -> Line -> [Cell]
+neighbors target (Line cells) = (removeMiddle.(neighborhood target)) cells
+
+countNeighbors :: Grid -> Int -> Int -> Int
+countNeighbors (Grid ((Line (Dead:_)):_)) x y = 0
+countNeighbors (Grid ((Line (Alive:_)):_)) x y = 1
+
+nextCellState :: Cell -> Int -> Cell
+nextCellState Alive 2 = Alive
+nextCellState _ 3 = Alive
+nextCellState _ _ = Dead
+
+nextGen :: Grid -> Grid
+nextGen grid = grid
 
 gameOfLife :: String
 gameOfLife = show(Grid([Line([Alive, Dead, Alive]), Line([Alive, Dead, Alive]), Line([Alive, Dead, Alive])]))
+
+takeFrom :: Int -> Int -> [a] -> [a]
+takeFrom start stop = (take stop).(drop start)
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,11 +4,17 @@ import Test.Hspec (Spec, hspec, describe, it, shouldBe)
 
 main :: IO ()
 main = hspec $ do
+  describe "Take From" $ do
+    it "should take the array from start" $ do
+      (takeFrom 1 2 [1, 2, 3, 4]) `shouldBe` [2, 3]
+
   describe "Cell" $ do
     it "should display an alive cell" $ do
         show(Alive) `shouldBe` "O"
     it "should display an dead cell" $ do
         show(Dead) `shouldBe` "_"
+
+
   describe "Grid" $ do
     it "should display a grid of one dead Cell" $ do
         show(Line([Dead])) `shouldBe` "_"
@@ -18,32 +24,55 @@ main = hspec $ do
         show(Line([Dead, Dead, Alive])) `shouldBe` "__O"
     it "should display a grid of multiple lines of Cells" $ do
         show(Grid([Line([Dead, Dead, Alive]), Line([Alive, Dead, Alive])])) `shouldBe` "__O\nO_O"
+
+
   describe "Rules" $ do
     describe "Rule of Solitude" $ do
       it "living cell with no neighbors dies" $ do
-        (nextGen Alive 0) `shouldBe` Dead
+        (nextCellState Alive 0) `shouldBe` Dead
       it "living cell with one neighbors dies" $ do
-        (nextGen Alive 1) `shouldBe` Dead
+        (nextCellState Alive 1) `shouldBe` Dead
     describe "Rule of Overpopulation" $ do
       it "living cell with four neighbors dies" $ do
-        (nextGen Alive 4) `shouldBe` Dead
+        (nextCellState Alive 4) `shouldBe` Dead
       it "living cell with five neighbors dies" $ do
-        (nextGen Alive 5) `shouldBe` Dead
+        (nextCellState Alive 5) `shouldBe` Dead
       it "living cell with six neighbors dies" $ do
-        (nextGen Alive 6) `shouldBe` Dead
+        (nextCellState Alive 6) `shouldBe` Dead
       it "living cell with seven neighbors dies" $ do
-        (nextGen Alive 7) `shouldBe` Dead
+        (nextCellState Alive 7) `shouldBe` Dead
       it "living cell with eight neighbors dies" $ do
-        (nextGen Alive 8) `shouldBe` Dead
+        (nextCellState Alive 8) `shouldBe` Dead
     describe "Rule surviving cells" $ do
       it "living cell with two neighbors survive" $ do
-        (nextGen Alive 2) `shouldBe` Alive
+        (nextCellState Alive 2) `shouldBe` Alive
       it "living cell with three neighbors survive" $ do
-        (nextGen Alive 3) `shouldBe` Alive
+        (nextCellState Alive 3) `shouldBe` Alive
     describe "Rule newborn cells" $ do
       it "dead cell with three neighbors is alive" $ do
-        (nextGen Dead 3) `shouldBe` Alive
+        (nextCellState Dead 3) `shouldBe` Alive
       it "dead cell with two neighbors should stay dead" $ do
-        (nextGen Dead 2) `shouldBe` Dead
+        (nextCellState Dead 2) `shouldBe` Dead
       it "dead cell with four neighbors should stay dead" $ do
-        (nextGen Dead 4) `shouldBe` Dead
+        (nextCellState Dead 4) `shouldBe` Dead
+
+  describe "Evolving" $ do
+    describe "Next Generation computation" $ do
+      it "dead cell with three neighbors is alive" $ do
+        nextGen(Grid([Line([Dead, Alive, Dead]), Line([Dead, Alive, Dead]), Line([Dead, Alive, Dead])])) `shouldBe` Grid([Line([Dead, Dead, Dead]), Line([Alive, Alive, Alive]), Line([Dead, Dead, Dead])])
+  
+  describe "Find Neighbors" $ do
+    it "Should remove target" $ do
+      let line = Line [Alive, Dead, Alive, Dead, Dead]
+        in (neighbors 2 line) `shouldBe` [Dead, Dead]
+
+  describe "Count alive Neighbors" $ do
+    describe "a single line Grid" $ do
+      it "should have no neighbors" $ do
+        let grid = (Grid [(Line [Dead, Alive, Dead]) ]) 
+          in (countNeighbors grid 0 1) `shouldBe` 0
+      it "should have one neighbor" $ do
+        let grid = (Grid [(Line [Dead, Dead, Alive, Alive, Dead, Alive]) ]) 
+          in (countNeighbors grid 0 3) `shouldBe` 1
+        
+ 
